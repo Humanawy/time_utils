6ba59389a7651c192b8a23511f9c27839d219604
Kacper <kacper.ryszawy@gmail.com>
2025-07-22 21:59:58 +0200

    utworzenie plików konfiguracyjnych do pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="time_units_package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Kacper",
+    description="Moduł do pracy z jednostkami czasu (godziny, dni, miesiące, itd.)",
+    python_requires=">=3.7",
+)
